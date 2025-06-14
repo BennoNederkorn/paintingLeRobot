f9c0dcd3324898b51301763ba178aa2a483f7d29
Benno Nederkorn <benno.nederkorn@osnanet.de>
2025-06-14 07:40:30 +0200

    1st structure

--- src/main.py
+++ src/main.py
@@ -1,9 +1,36 @@
-def speech_to_text() -> str:
-    print("Hallo Lee")
+import numpy as np
+
+def execute_recording() -> list[float]:
+    """
+    This function strarts a recording while a key is pressed and returns a .wav file/data
+    """
+    return []
+
+
+def speech_to_text(data : list[float]) -> str:
+    """
+    This function gets a speach data and returns text
+    """
+    
+    new_prompt : str = "Hello Lee" 
+    print(new_prompt)
+    return new_prompt
+
+def generate_image(prompt : str) -> np.ndarray:
+    return np.random.rand(255, 255, 3)
+
+def create_segmentated_image(image : np.ndarray) -> np.ndarray:
+    return np.random.rand(255, 255, 3)
 
 
 def main():
-    speech_to_text()
+    has_robot_finished_image = True
+    while(has_robot_finished_image):
+        new_speach_prompt : list[float] = execute_recording()
+        new_text_prompt : str = speech_to_text(new_speach_prompt)
+        image : np.ndarray = generate_image(new_text_prompt)
+        segmentated_image : np.ndarray = create_segmentated_image(image)
+
 
 
 
