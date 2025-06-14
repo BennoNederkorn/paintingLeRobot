16f7e79e6df3cf4d39501de9e34a06b5d50553c2
Benno Nederkorn <benno.nederkorn@osnanet.de>
2025-06-14 11:36:18 +0200

    4th structure with simpler linear pipeline

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Optional
 
 import speech_processing 
 import image_generation
@@ -7,22 +8,17 @@ from robot_controller import RobotController
 def main():
     robot_controller = RobotController()
 
-    robot_controller.start_control_loop() # starts Thread
-
-    while(not robot_controller.has_robot_finished_image()): 
-        new_speach_prompt : list[float] = speech_processing.execute_recording()
-        if new_speach_prompt == []:
-            # continue
-            pass
-
-        new_text_prompt : str = speech_processing.speech_to_text(new_speach_prompt)
-        painted_regions = robot_controller.get_painted_regions()
-        image : np.ndarray = image_generation.generate_image(new_text_prompt, painted_regions)
-        segmentated_image : np.ndarray = image_generation.create_segmentated_image(image)
-        robot_controller.generate_path(segmentated_image)
-        robot_controller.draw()
-
-    robot_controller.stop_control_loop()
+    # robot_controller.start_control_loop() # starts Thread
+    # while(not robot_controller.has_robot_finished_image()): # change this later to a while loop for 
+    
+    new_speach_prompt : Optional[list[float]] = speech_processing.execute_recording()
+    # TODO check if speech processing failed
+    new_text_prompt : str = speech_processing.speech_to_text(new_speach_prompt)
+    painted_regions = robot_controller.get_painted_regions()
+    image : np.ndarray = image_generation.generate_image(new_text_prompt, painted_regions)
+    segmentated_image : np.ndarray = image_generation.create_segmentated_image(image)
+    color_maps : list[np.ndarray] = image_generation.create_color_maps(segmentated_image)
+    robot_controller.start_control_loop(segmentated_image)
 
 
 
