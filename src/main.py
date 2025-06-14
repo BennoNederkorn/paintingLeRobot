688dc38e1de1e9b0d7b39b9fe5f339afcf0042ae
Benno Nederkorn <benno.nederkorn@osnanet.de>
2025-06-14 08:34:50 +0200

    2st structure

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+import RobotController from robot_controller
+
 def execute_recording() -> list[float]:
     """
     This function strarts a recording while a key is pressed and returns a .wav file/data
@@ -16,7 +18,8 @@ def speech_to_text(data : list[float]) -> str:
     print(new_prompt)
     return new_prompt
 
-def generate_image(prompt : str) -> np.ndarray:
+def generate_image(prompt : str, image : np.ndarray ) -> np.ndarray:
+
     return np.random.rand(255, 255, 3)
 
 def create_segmentated_image(image : np.ndarray) -> np.ndarray:
@@ -24,12 +27,18 @@ def create_segmentated_image(image : np.ndarray) -> np.ndarray:
 
 
 def main():
-    has_robot_finished_image = True
-    while(has_robot_finished_image):
+    robot_controller = RobotController()
+
+    robot_controller.start_control_loop() # starts Thread
+
+    while(robot_controller.has_robot_finished_image()):
         new_speach_prompt : list[float] = execute_recording()
         new_text_prompt : str = speech_to_text(new_speach_prompt)
+        painted_regions = robot_controller.get_painted_regions()
         image : np.ndarray = generate_image(new_text_prompt)
         segmentated_image : np.ndarray = create_segmentated_image(image)
+
+    robot_controller.stop_control_loop()
 
 
 
