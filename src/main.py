a6bfca02055247cab1d4c717ff49a0e43378b5dc
Mats Grobe <go36bil@mytum.de>
2025-06-14 09:02:00 +0200

    rename

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-import speech_prorcessing 
+import src.speech_processing as speech_processing 
 from robot_controller import RobotController 
 
 def generate_image(prompt : str, image : np.ndarray ) -> np.ndarray:
@@ -17,8 +17,8 @@ def main():
     robot_controller.start_control_loop() # starts Thread
 
     while(robot_controller.has_robot_finished_image()):
-        new_speach_prompt : list[float] = speech_prorcessing.execute_recording()
-        new_text_prompt : str = speech_prorcessing.speech_to_text(new_speach_prompt)
+        new_speach_prompt : list[float] = speech_processing.execute_recording()
+        new_text_prompt : str = speech_processing.speech_to_text(new_speach_prompt)
         painted_regions = robot_controller.get_painted_regions()
         image : np.ndarray = generate_image(new_text_prompt, painted_regions)
         segmentated_image : np.ndarray = create_segmentated_image(image)
