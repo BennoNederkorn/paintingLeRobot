ebeffc70fe116ef29fdf7476d76f97e7189b4d66
Benno Nederkorn <benno.nederkorn@osnanet.de>
2025-06-15 11:04:08 +0200

    changed motor control file

--- test/test_vibe_movement.py
+++ test/test_vibe_movement.py
@@ -2,36 +2,56 @@
 
 from lerobot.common.robots.so101_follower.so101_follower import SO101Follower
 from lerobot.common.robots.so101_follower.config_so101_follower import SO101FollowerConfig
+from lerobot.common.motors import MotorCalibration
 import time
+import json
 
 def send_action_to_follower():
+
     # Create configuration - you may need to adjust these parameters
     config = SO101FollowerConfig(
-        port="/dev/ttyACM1",  # Adjust to your actual port
+        port="/dev/ttyACM0",  # Adjust to your actual port
         use_degrees=True,     # Set to False if you want to use range -100 to 100
         max_relative_target=None,  # Set a value like 30 if you want to limit movement speed
-        disable_torque_on_disconnect=False,
-        cameras={}  # Empty if no cameras needed for this action
+        disable_torque_on_disconnect=True,
+        cameras={},  # Empty if no cameras needed for this action
     )
-    
+
+
+    # Load calibration
+    with open("utils/joint_calibration.json", "r") as f:
+        calibration_data = json.load(f)
+    # print(f"calibration: {calibration_data}")
+    # config.calibration = calibration_data
+
     # Create robot instance
     robot = SO101Follower(config)
+    robot.bus.calibration : dict[str, MotorCalibration] = calibration_data
     
+    # calibration
+    # class MotorCalibration:
+    #     id: int
+    #     drive_mode: int
+    #     homing_offset: int
+    #     range_min: int
+    #     range_max: int
+
+
     try:
         # Connect to the robot
         print("Connecting to SO101 Follower...")
-        robot.connect(calibrate=False)  # Set to True if you need calibration
+        robot.connect(calibrate=True)  # Set to True if you need calibration
         print("Connected successfully!")
         
         # Define your specific action
         # Each joint position should be in degrees (if use_degrees=True) or -100 to 100 range
         action = {
             "shoulder_pan.pos": 0,    # Adjust these values as needed
-            "shoulder_lift.pos": 0,
-            "elbow_flex.pos": 0,
+            "shoulder_lift.pos": 100,
+            "elbow_flex.pos": -100,
             "wrist_flex.pos": 0,
             "wrist_roll.pos": 0,
-            "gripper.pos": 50,        # Gripper is always 0-100 range
+            "gripper.pos": 0,        # Gripper is always 0-100 range
 
         }
         
